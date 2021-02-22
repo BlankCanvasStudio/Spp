033e7584340291c23224a405fec2b784c762bf52
spencerstingley <laptop@bcs.com>
2021-02-21 22:43:54 -0600

    Began the Clean up of the code base. Added Vector and Matrix preliminary definitions. Added Vector operators. And added tictoc functions.

--- globals.h
+++ globals.h
@@ -23,8 +23,8 @@ bool Indexable(int i, pos len){
 struct Vector {
     num* array;
     pos len;
-    Vector(pos len){
-        length = len;
+    Vector(pos length){
+        len = length;
         array = new num[len];
     }
     num &operator[](int i) {
@@ -60,3 +60,26 @@ struct Matrix {
          }
     }
 };
+
+
+// Some Helpful Constructors 
+
+template<typename T, typename V>
+Vector VectorFull(T len, V value) {
+	Vector toReturn;
+	if (len < 0.0) { cout << "ERROR ENCOUNTERED" << endl; throw invalid_argument("Size of the vector must be positive."); }
+	for (unsigned i = 0; i<unsigned(len); i++) { toReturn.push_back(num(value)); }
+	return toReturn;
+}
+
+template<typename T, typename V, typename C>
+Matrix MatrixFull(T row, V col, C val) {
+    num value = num(val);
+    Matrix toReturn(row, col);
+    for( pos i=0; i<row; i++) {
+        for( pos j=0; j<col; j++) {
+            toReturn[i][j] = value;
+        }
+    }
+    return toReturn;
+}
--- operators.h
+++ operators.h
@@ -3,30 +3,30 @@
 using namespace std;
 
 //Vector Operators
-Vector operator - (Vector a, Vector b) {
-	pos lenA = a.size();
-	if (lenA != b.size()) {
+Vector operator - (Vector &a, Vector &b) {
+	pos lenA = a.len;
+	if (lenA != b.len) {
 		cout << "ERROR ENCOUNTERED" << endl; 
         throw invalid_argument("Vectors must have the same dimensions.");
 	}
-	Vector result = new Vector[lenA];
+	Vector result = new Vector(lenA);
 	for (pos i = 0; i < lenA; i++) {
 		result[i] = a[i] - b[i];
 	}
 	return result;
 }
-Vector operator - (Vector a) {
-	pos lenA = a.size();
-	Vector result = new Vector[lenA];
+Vector operator - (Vector &a) {
+	pos lenA = a.len;
+	Vector result = new Vector(lenA);
 	for (unsigned i = 0; i < lenA; i++) {
 		result[i] = -a[i];
 	}
 	return result;
 }
 template <typename T>
-Vector operator - (Vector a, T b) {
-	pos lenA = a.size();
-	Vector result = new Vector[lenA];
+Vector operator - (Vector &a, T &b) {
+	pos lenA = a.len;
+	Vector result = new Vector(lenA);
 	num num_b = num(b);
 		// Verify it is a num so c++ doesn't throw run time errors
 	for (unsigned i = 0; i < lenA; i++) {
@@ -36,10 +36,10 @@ Vector operator - (Vector a, T b) {
 }
 template<typename T>
 // This should be true false but with a 0/1 array of ints it'll be fine
-Vector operator > (Vector a, T b) {
+Vector operator > (Vector &a, T &b) {
 	pos lenA = a.size();
 	num num_b = num(b);
-	vector greaterThan = new vector[lenA];
+	vector greaterThan = new Vector(lenA);
 	for (pos i = 0; i < lenA; i++) {
 		if (a[i] > num_b) { a[i] = 0; }
 		else{ a[i] = 1; }
@@ -47,14 +47,99 @@ Vector operator > (Vector a, T b) {
 	return greaterThan;
 }
 template<typename T>
-vector<int> operator < (T b, Vector a) {
+vector<int> operator < (T &b, Vector &a) {
 	// Hopefully this isn't too slow
 	return a > b;
 }
+Vector operator < (Vector &a, T &b) {
+	pos lenA = a.size();
+	num num_b = num(b);
+	vector greaterThan = new Vector(lenA);
+	for (pos i = 0; i < lenA; i++) {
+		if (a[i] < num_b) { a[i] = 0; }
+		else{ a[i] = 1; }
+	}
+	return greaterThan;
+}
+template<typename T>
+vector<int> operator > (T &b, Vector &a) {
+	// Hopefully this isn't too slow
+	return a < b;
+}
+template <typename T>
+Vector operator * (Vector &a, T &b) {
+	pos lenA = a.len;
+	num val = num(b);
+	Vector result(lenA);
+		// Lets hope that this works
+	for (pos i = 0; i < lenA; i++) {
+		result[i] = a[i] * val;
+	}
+	return result;
+}
+template<typename T>
+Vector operator * (T &b, Vector &a) {
+	return a * b;
+}
+Vector operator * (Vector &a, Vector &b) {
+	pos lenA = a.len;
+	pos lenB = b.len;
+	if (lenA != lenB) { 
+		cout << "ERROR OCCURRED. DIMS DONT MATCH" << endl; 
+		throw invalid_argument("Dimensions don't match in vector multiplication."); 
+	}
+	Vector toReturn(lenA);
+	for (int i = 0; i < lenA; i++) {
+		toReturn[i] = a[i] * b[i];
+	}
+	return toReturn;
+}
+template<typename T>
+Vector operator / (Vector &a, T &b) {
+	pos lenA = a.len;
+	num val = num(b);
+	Vector toReturn(lenA);
+	for (pos i = 0; i < lenA; i++) {
+		toReturn[i] = a[i] / val;
+	}
+	return toReturn;
+}
+template<typename T>
+Vector operator / (T b, Vector a) {
+	pos lenA = a.len;
+	num val = num(b);
+	Vector toReturn(lenA);
+	for (pos i = 0; i < lenA; i++) {
+		toReturn[i] = val / a[i];
+	}
+	return toReturn;
+}
+template<typename T>
+Vector operator == (Vector a, T value) {
+	pos lenA = a.len;
+	Vector toReturn(lenA);
+	for (unsigned i = 0; i < lenA; i++) {
+		if (a[i] == value) { toReturn[i] = 1; }
+		else { toReturn[i] = 0; }
+	}
+	return toReturn;
+}
+template<typename T>
+Vector operator == (T value, Vector a) {
+	return a == value;
+}
+
+bool operator (Vector a, Vector b) {
+	pos lenA = a.len;
+	if(lenA != b.len){ throw invalid_argument("Vectors must have the same dimensions."); }
+	for( int i=0; i<lenA; i++){
+		if (a[i]!=b[i]){return false;}
+	}
+	return true;
+}
 
 
-
-
+// Matrix Operators
 
 
 
